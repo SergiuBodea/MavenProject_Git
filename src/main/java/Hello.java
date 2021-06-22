3e14c7ccbb0b651d18c628231c29d3c4b47637b0
SergiuBodea <serg.bodea@gmail.com>
2021-06-22 17:17:28 +0300

    pdate commit in local repo

--- src/main/java/Hello.java
+++ src/main/java/Hello.java
@@ -2,5 +2,8 @@ public class Hello {
     public static void main(String[] args) {
         System.out.println("Hello");
         System.out.println("Developing hello feature");
+        System.out.println("Local Reepo is already published on Github");
+        System.out.println("Work");
+
     }
 }
